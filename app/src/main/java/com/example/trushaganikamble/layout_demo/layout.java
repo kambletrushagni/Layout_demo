ff8f907712dc215803b85bb237f18c5230c66600
trushagnikamble <trushaganikamble1@gmail.com>
2018-04-10 14:16:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/trushaganikamble/layout_demo/layout.java
@@ -0,0 +1,14 @@
+package com.example.trushaganikamble.layout_demo;
+
+import android.app.Activity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+    }
+}
